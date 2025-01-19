bff9c64b8712ac35a6759905d1fc9c68f4c615f3
Dominik Dudek <dom-dud774@o2.pl>
2025-01-19 23:11:12 +0100

    Solved typo and changed function names

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import postsFromServer from './api/posts';
 import commentsFromServer from './api/comments';
 import usersFromServer from './api/users';
 import { User } from './types/User';
-import { Comment } from './types/Comment.';
+import { Comment } from './types/Comment';
 import { Post } from './types/Post';
 import { PostList } from './components/PostList';
 
--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,4 +1,4 @@
-import { Comment } from './Comment.';
+import { Comment } from './Comment';
 import { User } from './User';
 
 export interface Post {
